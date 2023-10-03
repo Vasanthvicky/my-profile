d06590a520605069d739705b38691520c1583d99
vignesh vasanth <vigneshvasanth.chandrasekar@trustrace.com>
2023-10-03 22:43:28 +0530

    router module and sid and top and page loader component is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { RouterComponent } from './router/router.component';
+import { FullPageLoaderComponent } from './full-page-loader/full-page-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    SideNavComponent,
+    TopNavigationComponent,
+    RouterComponent,
+    FullPageLoaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
